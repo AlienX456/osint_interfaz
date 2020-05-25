44adc708634060b9469b061d193441c8e3fbb1df
alienx456 <estebanelias27@gmail.com>
2020-05-25 12:32:37 -0500

    se añade caracteristica overflow para la tabla

--- src/App.js
+++ src/App.js
@@ -176,12 +176,25 @@ function App() {
           </div>
         </div>
         
-        <div className="flex">
+        <div className="flex overflow-auto">
           <table className="table-auto mx-auto my-auto border-2">
             <thead>
               <tr>
                 <th className="px-4 py-2">Title</th>
                 <th className="px-4 py-2">Author</th>
+                <th className="px-4 py-2">Title</th>
+                <th className="px-4 py-2">Author</th>
+                <th className="px-4 py-2">Title</th>
+                <th className="px-4 py-2">Author</th>
+                <th className="px-4 py-2">Views</th>
+                <th className="px-4 py-2">Views</th>
+                <th className="px-4 py-2">Views</th>
+                <th className="px-4 py-2">Views</th>
+                <th className="px-4 py-2">Views</th>
+                <th className="px-4 py-2">Views</th>
+                <th className="px-4 py-2">Views</th>
+                <th className="px-4 py-2">Views</th>
+                <th className="px-4 py-2">Views</th>
                 <th className="px-4 py-2">Views</th>
               </tr>
             </thead>
@@ -189,17 +202,55 @@ function App() {
               <tr>
                 <td className="border px-4 py-2">Intro to CSS</td>
                 <td className="border px-4 py-2">Adam</td>
+                <td className="border px-4 py-2">Intro to CSS</td>
+                <td className="border px-4 py-2">Adam</td>
+                <td className="border px-4 py-2">Intro to CSS</td>
+                <td className="border px-4 py-2">Adam</td>
+                <td className="border px-4 py-2">858</td>
+                <td className="border px-4 py-2">858</td>
+                <td className="border px-4 py-2">858</td>
+                <td className="border px-4 py-2">858</td>
+                <td className="border px-4 py-2">858</td>
+                <td className="border px-4 py-2">858</td>
+                <td className="border px-4 py-2">858</td>
+                <td className="border px-4 py-2">858</td>
+                <td className="border px-4 py-2">858</td>
                 <td className="border px-4 py-2">858</td>
               </tr>
               <tr className="bg-gray-100">
                 <td className="border px-4 py-2">A Long and Winding Tour of the History of UI Frameworks and Tools and the Impact on Design</td>
                 <td className="border px-4 py-2">Adam</td>
+                <td className="border px-4 py-2">A Long and Winding Tour of the History of UI Frameworks and Tools and the Impact on Design</td>
+                <td className="border px-4 py-2">Adam</td>
+                <td className="border px-4 py-2">A Long and Winding Tour of the History of UI Frameworks and Tools and the Impact on Design</td>
+                <td className="border px-4 py-2">Adam</td>
                 <td className="border px-4 py-2">112</td>
+                <td className="border px-4 py-2">858</td>
+                <td className="border px-4 py-2">858</td>
+                <td className="border px-4 py-2">858</td>
+                <td className="border px-4 py-2">858</td>
+                <td className="border px-4 py-2">858</td>
+                <td className="border px-4 py-2">858</td>
+                <td className="border px-4 py-2">858</td>
+                <td className="border px-4 py-2">858</td>
               </tr>
               <tr>
                 <td className="border px-4 py-2">Intro to JavaScript</td>
                 <td className="border px-4 py-2">Chris</td>
+                <td className="border px-4 py-2">Intro to JavaScript</td>
+                <td className="border px-4 py-2">Chris</td>
+                <td className="border px-4 py-2">Intro to JavaScript</td>
+                <td className="border px-4 py-2">Chris</td>
                 <td className="border px-4 py-2">1,280</td>
+                <td className="border px-4 py-2">858</td>
+                <td className="border px-4 py-2">858</td>
+                <td className="border px-4 py-2">858</td>
+                <td className="border px-4 py-2">858</td>
+                <td className="border px-4 py-2">858</td>
+                <td className="border px-4 py-2">858</td>
+                <td className="border px-4 py-2">858</td>
+                <td className="border px-4 py-2">858</td>
+                <td className="border px-4 py-2">858</td>
               </tr>
             </tbody>
           </table>
